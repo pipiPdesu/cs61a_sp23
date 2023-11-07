795e058707044f45a9ed715489c70b4e38e3f5e6
pipiPdesu <xinghr@seu.edu.cn>
2023-11-07 22:49:19 +0800

    week13

---
+++ hw/hw08/hw08.scm
@@ -0,0 +1,17 @@
+(define (square n) (* n n))
+
+(define (pow base exp) (if (zero? exp) 1 (if(zero? (modulo exp 2))
+                           (square (pow base (/ exp 2)))
+                           (* base (pow base (- exp 1))))))
+
+(define (repeatedly-cube n x)
+  (if (zero? n)
+      x
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y))))
+
+(define (cddr s) (cdr (cdr s)))
+
+(define (cadr s) (car (cdr s)))
+
+(define (caddr s) (car (cdr (cdr s))))
---
+++ labs/lab11/lab11.scm
@@ -0,0 +1,25 @@
+(define (over-or-under num1 num2) (
+                                  
+  cond
+    ((= num1 num2) 0)
+    ((< num1 num2) -1)
+    (else 1)))
+
+(define (make-adder num) (lambda (x) (+ x num)))
+
+(define (composed f g) (lambda (x) (f (g x))))
+
+(define (repeat f n) (if (= n 0) (lambda (x) x) (composed f (repeat f (- n 1)))))
+
+(define (max a b)
+  (if (> a b)
+      a
+      b))
+
+(define (min a b)
+  (if (> a b)
+      b
+      a))
+
+(define (gcd a b) (if (= b 0) a (gcd b (remainder a b))))
+
